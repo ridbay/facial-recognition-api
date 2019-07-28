2d40f6510f7915539329cad5620ab540df4e82c8
Balogun Ridwan <balogunridwan@gmail.com>
2019-07-28 07:46:28 +0100

    update database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-parallel-63775',
         user: 'postgres',
         password: 'test',
         database: 'facial'
